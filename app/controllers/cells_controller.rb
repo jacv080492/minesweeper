57de11147763689da0334aec5b3760e2c264e630
jacv080492 <jacv80492@gmail.com>
2022-04-10 23:23:24 -0700

    Http requests methods for Cells controller
    before actions addeded and show, create, update verbs addeded

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,6 +1,39 @@
 class CellsController < ApplicationController
+    before_action :set_cell, only: %i[ show update]
+
     def index
         @cells = Cell.all
         render json: @cells
+    end
+
+    def show
+        render json: @cell
+    end
+
+    def create
+        @cell = Cell.new(cell_params)
+        if @cell.save
+          render json: @cell, status: :created, location: @cell
+        else
+          render json: @cell.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @cell.update(cell_params)
+          render json: @cell
+        else
+          render json: @cell.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def set_cell
+      @cell = Cell.find(params[:id])
+    end
+
+    private
+    def cell_params
+      params.require(:cell).permit(:x_axis, :y_axis, :is_mined, :is_exposed, :is_flagged)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :games
-
-  get '/cells', to: 'cells#index'
+  resources :cells
 end
