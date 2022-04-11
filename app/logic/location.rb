aebafe9ed552de4fcfb73a56745d0786f65b6db5
jacv080492 <jacv80492@gmail.com>
2022-04-10 23:41:44 -0700

    class location addeded
    This class will save my coordinates, it will be the base class of mine and cell

---
+++ app/logic/location.rb
@@ -0,0 +1,12 @@
+class Location
+    attr_reader :x_axis, :y_axis
+  
+    def initialize(x_axis, y_axis)
+      @x_axis = x_axis
+      @y_axis = y_axis
+    end
+  
+    def to_s
+      "(#{x_axis}, #{y_axis})"
+    end
+end
