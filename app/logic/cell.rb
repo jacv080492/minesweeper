8f1b2992be1db825b79527a83d9bdeea2c4ac810
jacv080492 <jacv80492@gmail.com>
2022-04-10 23:50:54 -0700

    cell class added
    it represents a cell in  my business logic

---
+++ app/logic/cell.rb
@@ -0,0 +1,20 @@
+require_relative './location'
+
+class Cell < Location
+    attr_reader :x_axis, :y_axis, :is_mined, :adyacent_cells
+    attr_accessor :is_exposed, :is_flagged
+
+    def initialize(x_axis, y_axis, is_mined)
+        @x_axis = x_axis
+        @y_axis = y_axis
+        @is_mined = is_mined
+        @is_exposed = false
+        @is_flagged = false
+        @adyacent_cells = []
+    end
+
+  public
+  def add_adyacent(adyacent_cell)
+    @adyacent_cells.push(adyacent_cell)
+  end
+end
